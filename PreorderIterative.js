3638557ebdeb52b00120151e22e98fca132ff86e
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-05 08:43:57 +0530

    Create PreorderIterative.js
    Tree | pre order | iterative

---
+++ PreorderIterative.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    if (!root) {
+        return [];
+    }
+    var result = [];
+    let stack = [];
+    stack.push(root);
+    while (stack.length > 0) {
+        let node = stack.pop();
+        result.push(node.val);
+        if (node.right != null) {
+            stack.push(node.right);
+        }
+        if (node.left != null) {
+            stack.push(node.left);
+        }
+    }
+    return result;
+};
