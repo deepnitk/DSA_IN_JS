d9c4f7d573f86f15e0b4ea0cd55395ab79b450b0
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-12 08:40:36 +0530

    Update NoOfIsland.java
    Graph | BFS for Island problem

--- NoOfIsland.java
+++ NoOfIsland.java
@@ -38,3 +38,65 @@ class Solution {
         }
     }
 }
+
+//BFS Solution
+// TC: O(n*m)
+// SC: O(n*m) + O(n*m) 
+class Pair {
+    int first;
+    int second;
+    public Pair(int _first, int _second) {
+        this.first = _first;
+        this.second = _second;
+    }
+}
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int noOfIsland = 0;
+        int[][] vis = new int[n][m];
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (vis[row][col] == 0 && grid[row][col] == '1') {
+                    noOfIsland++;
+                    bfs(row, col, vis, grid);
+                }
+            }
+        }
+        return noOfIsland;
+    }
+
+    private void bfs(int ro, int co, int[][] vis, char[][] grid) {
+        vis[ro][co] = 1;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(ro, co));
+        int n = grid.length;
+        int m = grid[0].length;
+
+        while (!q.isEmpty()) {
+            int row = q.peek().first;
+            int col = q.peek().second;
+            q.remove();
+
+            if(row - 1 >= 0 && grid[row - 1][col] == '1' && vis[row - 1][col] == 0) {
+                vis[row - 1][col] = 1;
+                q.add(new Pair(row - 1, col));
+            }
+            if(row + 1 < n && grid[row + 1][col] == '1' && vis[row + 1][col] == 0) {
+                vis[row + 1][col] = 1;
+                q.add(new Pair(row + 1, col));
+            }
+            if(col - 1 >= 0 && grid[row][col - 1] == '1' && vis[row][col - 1] == 0) {
+                vis[row][col - 1] = 1;
+                q.add(new Pair(row, col - 1));
+            }
+            if(col + 1 < m && grid[row][col + 1] == '1' && vis[row][col + 1] == 0) {
+                vis[row][col + 1] = 1;
+                q.add(new Pair(row, col + 1));
+            }
+        }
+    }
+
+}
