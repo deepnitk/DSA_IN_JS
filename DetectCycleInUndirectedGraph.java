d2f49535780c305bf6a97476495974945aadd5b5
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-14 21:21:29 +0530

    Update DetectCycleInUndirectedGraph.java

--- DetectCycleInUndirectedGraph.java
+++ DetectCycleInUndirectedGraph.java
@@ -75,3 +75,75 @@ class Solution {
         return false;
     }
 }
+
+//DFS solution
+
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+class Pair {
+    int first;
+    int second;
+    public Pair(int _first, int _second){
+        this.first = _first;
+        this.second = _second;
+    }
+}
+class Solution {
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean vis[] = new boolean[V];
+        for (int i = 0;i < V;i++) {
+            if (!vis[i]) {
+                if(checkForCycleDFS(i, -1, adj, vis)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean checkForCycleDFS(int src, int parent, ArrayList<ArrayList<Integer>> adj, boolean[] vis){
+        vis[src] = true;
+        for (int adjacentNode: adj.get(src)) {
+                if (!vis[adjacentNode]) {
+                    if(checkForCycleDFS(adjacentNode, src, adj, vis)) {
+                        return true;
+                    }
+                } else if (parent != adjacentNode) {
+                    return true;
+                }
+        }
+        return false;
+    }
+}
