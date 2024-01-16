9c4a2b88d1dcb43c2f8509aa9fce388c8041f4fb
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-16 18:09:33 +0530

    Update SecondLargest.java

--- SecondLargest.java
+++ SecondLargest.java
@@ -10,3 +10,36 @@ public class Solution {
         return res;
     }
 }
+
+//Better approach
+
+
+import java.util.Arrays;
+public class Solution {
+    public static int[] getSecondOrderElements(int n, int []a) {
+        // Write your code here.
+
+        int largest = a[0];
+        int smallest = a[0];
+        for (int i = 0; i < n; i++) {
+            if (a[i] > largest) {
+                largest = a[i];
+            }
+             if (a[i] < smallest) {
+                smallest = a[i];
+            }
+        }
+        int secLargest = -1;
+        int secSmallest = Integer.MAX_VALUE;
+        for(int i = 0; i< n; i++) {
+            if (a[i] > secLargest && a[i] != largest) {
+                secLargest = a[i];
+            }
+            if (a[i] < secSmallest && a[i] != smallest) {
+                secSmallest = a[i];
+            }
+        }
+        int[]res = {secLargest, secSmallest};
+        return res;
+    }
+}
