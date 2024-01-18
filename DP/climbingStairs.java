c61cb1a7688fa13ba8676e47ad16d0daafbef268
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-18 10:00:03 +0530

    Update climbingStairs.java

--- DP/climbingStairs.java
+++ DP/climbingStairs.java
@@ -14,3 +14,23 @@ class Solution {
         return oneStep + twoStep;
     }
 }
+
+//Memoization
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return f(n, dp);
+    }
+
+    private int f (int ind, int[] dp) {
+        if (ind <= 1) {
+            return 1;
+        }
+        if (dp[ind] != -1) {
+            return dp[ind];
+        }
+        return dp[ind] = f(ind - 1, dp) + f(ind - 2, dp);
+    }
+}
