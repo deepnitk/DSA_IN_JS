bb9e6722aae83f8291179836a59bb88b4c5e7519
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-18 20:40:30 +0530

    Update HouseRobber.java

--- DP/HouseRobber.java
+++ DP/HouseRobber.java
@@ -46,3 +46,29 @@ class Solution {
 
     }
 }
+
+//Tabulation approach
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return f(n, nums, dp);
+    }
+
+    private int f(int n, int[] nums, int[] dp) {
+        dp[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i];
+            if (i > 1) {
+                pick +=  dp[i - 2];
+            } 
+            int notPick = 0 + dp[i - 1];
+            dp[i] = Math.max(pick, notPick);
+        }
+        return dp[n - 1];
+
+    }
+}
