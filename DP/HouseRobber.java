3f576fccf4475dc2b5d3328e0ffeecfa0e0bc9f4
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-18 20:31:34 +0530

    Update HouseRobber.java

--- DP/HouseRobber.java
+++ DP/HouseRobber.java
@@ -19,3 +19,30 @@ class Solution {
 
     }
 }
+
+//Memoization approach
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return f(n - 1, nums, dp);
+    }
+
+    private int f(int idx, int[] nums, int[] dp) {
+        if (idx < 0) {
+            return 0;
+        }
+
+        if (idx == 0) {
+            return nums[0];
+        }
+        if (dp[idx] != -1) {
+            return dp[idx];
+        }
+        int pick = nums[idx] + f(idx - 2, nums, dp);
+        int notPick = 0 + f(idx - 1, nums, dp);
+        return dp[idx] = Math.max(pick, notPick);
+
+    }
+}
