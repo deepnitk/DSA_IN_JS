8f4b406c633172d249ce6fd1d141698e0f2f05c3
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-18 23:45:36 +0530

    Update HouseRobber.java

--- DP/HouseRobber.java
+++ DP/HouseRobber.java
@@ -72,3 +72,25 @@ class Solution {
 
     }
 }
+
+//Space optimization
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int prev = nums[0];
+        int prev2 = 0;
+
+        for (int i = 1; i < n; i++) {
+            int pick = nums[i];
+            if (i > 1) {
+                pick +=  prev2;
+            } 
+            int notPick = 0 + prev;
+            int curri = Math.max(pick, notPick);
+            prev2 = prev;
+            prev = curri;
+        }
+        return prev;
+    }
+}
