acae0e00afd9c48bb2d3da3407221883bc0603d0
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-19 16:30:04 +0530

    Update MinPathSum.java

--- DP/MinPathSum.java
+++ DP/MinPathSum.java
@@ -51,3 +51,40 @@ class Solution {
          return dp[row][col] = Math.min(up, left);  
     }
 }
+
+//Tabulation approach
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] dp = new int[n][m];
+        for(int[] row: dp) {
+            Arrays.fill(row, -1);
+        }
+        return minPathSumUtil(n, m, grid, dp);
+    }
+
+    private int minPathSumUtil(int n, int m, int[][] grid, int[][] dp) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else {
+                    int  up = grid[i][j];
+                    if (i  > 0) 
+                        up += dp[i - 1][j];
+                    else
+                        up += (int) Math.pow(10, 9);
+                    int left = grid[i][j];
+                    if (j > 0) 
+                        left += dp[i][j - 1];
+                    else
+                        left += (int) Math.pow(10, 9);
+                    dp[i][j] = Math.min(up, left); 
+                    } 
+            }
+        }
+        return dp[n -1][m - 1];
+        
+    }
+}
