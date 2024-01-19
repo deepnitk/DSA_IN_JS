63d110d27a609d16323cff4a73191dc1124ef1fa
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-19 14:42:51 +0530

    Update UniquesPaths2.java

--- DP/UniquesPaths2.java
+++ DP/UniquesPaths2.java
@@ -37,3 +37,38 @@ class Solution {
         
     }
 }
+
+//Space optimization
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m =obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if (obstacleGrid[m - 1][n -1] == 1 || obstacleGrid[0][0] == 1) return 0;
+        int prev[] = new int[n];
+        for (int i = 0; i < m; i++) {
+            int temp[] = new int[n];
+            for (int j = 0; j < n; j++) {
+                // Base condition: If we are at the top-left cell (0, 0), there's one way to reach it.
+                if (i == 0 && j == 0) {
+                    temp[j] = 1;
+                    continue;
+                }
+
+                int up = 0;
+                int left = 0;
+
+                // Calculate the number of ways by moving up (if possible) and left (if possible)
+                if (i > 0 && obstacleGrid[i][j] != 1)
+                    up = prev[j];
+                if (j > 0 && obstacleGrid[i][j] != 1)
+                    left = temp[j - 1];
+
+                // Store the total number of ways to reach the current cell in the DP array
+                temp[j] = up + left;
+            }
+            prev = temp;
+        }
+        return prev[n - 1];
+    }
+
+}
