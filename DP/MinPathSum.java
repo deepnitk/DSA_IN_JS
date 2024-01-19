5bfd28b4faece56b7067330998a9e5efc8b97b77
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-19 16:33:57 +0530

    Update MinPathSum.java

--- DP/MinPathSum.java
+++ DP/MinPathSum.java
@@ -88,3 +88,43 @@ class Solution {
         
     }
 }
+
+//Space optimized approach
+
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[] prev = new int[m];
+        Arrays.fill(prev, -1);
+        return minPathSumUtil(n, m, grid, prev);
+    }
+
+    private int minPathSumUtil(int n, int m, int[][] grid, int[] prev) {
+        
+        for (int i = 0; i < n; i++) {
+            int[] temp = new int[m];
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) {
+                   temp[j] = grid[i][j];
+                } else {
+                    int  up = grid[i][j];
+                    if (i  > 0) 
+                        up += prev[j];
+                    else
+                        up += (int) Math.pow(10, 9);
+                    int left = grid[i][j];
+                    if (j > 0) 
+                        left += temp[j - 1];
+                    else
+                        left += (int) Math.pow(10, 9);
+
+                    temp[j] = Math.min(up, left); 
+                    } 
+            }
+            prev = temp;
+        }
+        return prev[m - 1];
+        
+    }
+}
