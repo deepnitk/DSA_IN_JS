f4c9531b68458e1852b8c3962ebf52d2541516e2
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-20 09:49:40 +0530

    Update TriangleMinPath.java

--- DP/TriangleMinPath.java
+++ DP/TriangleMinPath.java
@@ -46,3 +46,33 @@ class Solution {
         return dp[i][j] = Math.min(top, right);
     }
 }
+
+//Tabulation solution
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int m = triangle.get(n - 1).size();
+        int[][] dp = new int[n][m];
+        for (int[] row: dp) {
+            Arrays.fill(row, -1);
+        }
+        return f(triangle, dp);
+    }
+
+    private int f(List<List<Integer>> triangle, int[][] dp) {
+        int n = triangle.size();
+        int m = triangle.get(n - 1).size();
+        for(int j = 0; j < m; j++) {
+            dp[n - 1][j] = triangle.get(n - 1).get(j);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                int top = triangle.get(i).get(j) + dp[i + 1][j];
+                int right = triangle.get(i).get(j) + dp[i + 1][j + 1];
+                dp[i][j] = Math.min(top, right);
+            }
+        }
+        return dp[0][0];
+    }
+}
