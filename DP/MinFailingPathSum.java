59bf73ba2fd28902df593f72335b9fcbf19e4419
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-21 21:52:12 +0530

    Update MinFailingPathSum.java

--- DP/MinFailingPathSum.java
+++ DP/MinFailingPathSum.java
@@ -62,3 +62,46 @@ class Solution {
         return dp[i][j] = Math.min(left, Math.min(top, right));
     }
 }
+
+//Tabulation -- top down approach
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] dp = new int[n][m];
+        for (int[] row: dp) {
+            Arrays.fill(row, -1);
+        }
+
+        for (int j = 0; j < m; j++) {
+            dp[n - 1][j] = matrix[n - 1][j];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+
+                int left = matrix[i][j];
+                if (j - 1 >= 0) {
+                    left += dp[i + 1][j - 1];
+                } else {
+                    left += (int) Math.pow(10, 9);
+                }
+                int top = matrix[i][j] + dp[i + 1][j];
+                int right = matrix[i][j];
+                if (j + 1 < m) {
+                    right += dp[i + 1][j + 1];
+                } else {
+                    right +=  (int) Math.pow(10, 9);
+                }
+
+                dp[i][j] = Math.min(left, Math.min(top, right));
+            }
+        }
+
+        int mini = Integer.MAX_VALUE;
+        for (int j = 0; j < m; j++) {
+            mini = Math.min(mini, dp[0][j]);
+        }
+        return mini;       
+    }   
+
+}
