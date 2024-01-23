9d7613c7adeedd8827118df3eaf766df1adc3b46
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-23 09:16:55 +0530

    Update PartitionEqualSubsetSum.java

--- DP/PartitionEqualSubsetSum.java
+++ DP/PartitionEqualSubsetSum.java
@@ -63,3 +63,49 @@ class Solution {
         return notPick || pick;
     }
 }
+
+//Tabulation
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 == 1) {
+            return false;
+        }
+        int k = sum / 2;
+        boolean[][] dp = new boolean[n][k + 1];
+
+        for (boolean[] row: dp) {
+            Arrays.fill(row, false);
+        }
+
+        return subsetSum(n, nums, k, dp);
+    }
+
+    private boolean subsetSum(int n, int[] nums, int k, boolean[][] dp) {
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = true;
+        }
+        if (nums[0] <= k) {
+            dp[0][nums[0]] = true;
+        }
+
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= k; target++) {
+                boolean pick = false;
+                if (target > nums[ind]) {
+                    pick = dp[ind - 1][target - nums[ind]];
+                }
+                boolean notPick = dp[ind - 1][target];
+                dp[ind][target] = notPick || pick;
+            }
+        }
+
+        
+        return dp[n - 1][k];
+    }
+}
