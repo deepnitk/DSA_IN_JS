880d2356092176a1dfb41f1761a047306dcb07ae
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-24 21:01:52 +0530

    Update ConstructBST.java

--- BST/ConstructBST.java
+++ BST/ConstructBST.java
@@ -1,3 +1,4 @@
+//Best solution
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -25,6 +26,50 @@ class Solution {
        TreeNode root = new TreeNode(preorder[i[0]++]);
        root.left = bstFromPreorderUtil(preorder, root.val, i);
        root.right = bstFromPreorderUtil(preorder, bound, i);
+       return root;
+    }
+}
+
+//Naive solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        int[] inorder = preorder.clone();
+        Arrays.sort(inorder);
+        HashMap<Integer, Integer> iMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            iMap.put(inorder[i], i);
+        }
+        TreeNode root = buildTree(preorder, 0 , preorder.length - 1, inorder, 0, inorder.length - 1, iMap);
+        return root;
+    }
+
+    TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, HashMap<Integer, Integer> iMap) {
+       if (preStart > preEnd || inStart > inEnd) {
+           return null;
+       }
+
+       TreeNode root = new TreeNode(preorder[preStart]);
+       int inRoot = iMap.get(root.val);
+       int numsLeft = inRoot - inStart;
+
+       root.left = buildTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot - 1, iMap);
+       root.right = buildTree(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot + 1, inEnd, iMap);
        return root;
     }
 } 
