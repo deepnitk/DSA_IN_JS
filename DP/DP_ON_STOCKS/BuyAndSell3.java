ed1fe8fcb907cdb8198d01405822d4803a50d2e3
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-30 12:01:11 +0530

    Update BuyAndSell3.java

--- DP/DP_ON_STOCKS/BuyAndSell3.java
+++ DP/DP_ON_STOCKS/BuyAndSell3.java
@@ -73,3 +73,38 @@ class Solution {
         return dp[0][1][2];
     }
 }
+
+//Space optimization
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][] prev = new int[2][3];
+        return maxProfitUtil(prices, prev);
+    }
+
+    private int maxProfitUtil(int[] prices, int[][] prev) {
+        int n = prices.length;
+
+        for(int idx = n - 1; idx >= 0; idx--) {
+            int[][] curr = new int[2][3];
+            for (int buy = 1; buy >= 0; buy --) {
+                for (int cap = 2; cap > 0; cap --) {
+                    int profit = 0;
+                    // I can buy today
+                    if (buy == 1) {
+
+                        profit = Math.max((-prices[idx] + prev[0][cap]), //will buy
+                                            0 + prev[1][cap]); //not buy
+                    } else {  // I need to sell today
+                        profit = Math.max((prices[idx] + prev[1][cap - 1]), //will sell
+                                            0 + prev[0][cap]); //not sell
+                    }
+                   curr[buy][cap] = profit;
+                }
+            }
+            prev = curr;
+        }
+        
+
+        return prev[1][2];
+    }
+}
