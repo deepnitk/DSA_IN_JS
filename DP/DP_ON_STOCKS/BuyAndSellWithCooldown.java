41026fe73690cf33cba4f1f1543d5537de7a3714
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-30 14:55:21 +0530

    Update BuyAndSellWithCooldown.java

--- DP/DP_ON_STOCKS/BuyAndSellWithCooldown.java
+++ DP/DP_ON_STOCKS/BuyAndSellWithCooldown.java
@@ -26,3 +26,35 @@ class Solution {
         return  profit;
     }
 }
+
+//Tabulation
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        for(int[] row: dp)
+            Arrays.fill(row, -1);
+        return maxProfitUtil(0, 1, prices, dp);
+    }
+
+    private int maxProfitUtil(int idx, int buy, int[] prices, int[][] dp) {
+        int n = prices.length;
+        if (idx >= n) {
+            return  0;
+        }
+        if (dp[idx][buy] != -1) {
+            return dp[idx][buy];
+        }
+        int profit = 0;
+        // I can buy today
+        if (buy == 1) {
+
+            profit = Math.max((-prices[idx] + maxProfitUtil(idx + 1, 0, prices, dp)), //will buy
+                                0 + maxProfitUtil(idx + 1, 1, prices, dp)); //not buy
+        } else {  // I need to sell today
+            profit = Math.max((prices[idx] + maxProfitUtil(idx + 2, 1,  prices, dp)), //will sell
+                                0 + maxProfitUtil(idx + 1, 0, prices, dp)); //not sell
+        }
+
+        return  dp[idx][buy] = profit;
+    }
+}
