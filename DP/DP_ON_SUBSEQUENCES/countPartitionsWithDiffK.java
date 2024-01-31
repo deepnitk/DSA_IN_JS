950dbf6cb501352777343e54ce965975c3b32455
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-01-31 18:16:02 +0530

    Update countPartitionsWithDiffK.java

--- DP/DP_ON_SUBSEQUENCES/countPartitionsWithDiffK.java
+++ DP/DP_ON_SUBSEQUENCES/countPartitionsWithDiffK.java
@@ -35,3 +35,48 @@ class Solution{
         return pick + notPick;
     }
 }
+
+//Memoization solution
+class Solution{
+    int mod =(int)(Math.pow(10,9)+7);
+    public int countPartitions(int n, int d, int arr[]){
+        int totSum = 0;
+        for(int i=0; i<arr.length;i++){
+            totSum += arr[i];
+        }
+        
+        //Checking for edge cases
+        if(totSum-d<0) return 0;
+        if((totSum-d)%2==1) return 0;
+        
+        
+        int s2 = (totSum-d)/2;
+        
+        int[][] dp = new int[n][s2 + 1];
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        
+        return countPartitionsUtil(n - 1, arr, s2, dp);
+    }
+    
+    private int countPartitionsUtil(int ind, int[] nums, int target, int[][] dp) {
+        if(ind == 0){
+            if(target==0 && nums[0]==0)
+                return 2;
+            if(target==0 || target == nums[0])
+                return 1;
+            return 0;
+        }
+        
+        if (dp[ind][target] != -1) {
+            return dp[ind][target];
+        }
+        int pick = 0;
+        if (target >= nums[ind]) {
+            pick = countPartitionsUtil(ind - 1, nums, target - nums[ind], dp);
+        }
+        int notPick = countPartitionsUtil(ind - 1, nums, target, dp);
+        return dp[ind][target] = (pick + notPick)%mod;
+    }
+}
