550e87d5165dd5eac57e5d06e2223653a36afd8d
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-02-01 20:38:09 +0530

    Update coinChange2.java

--- DP/DP_ON_SUBSEQUENCES/coinChange2.java
+++ DP/DP_ON_SUBSEQUENCES/coinChange2.java
@@ -27,3 +27,39 @@ class Solution {
         return pick + notPick;
     }
 }
+
+//Memoization approach
+
+class Solution {
+    public int change(int T, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n][T + 1];
+        for(int[] row: dp) {
+            Arrays.fill(row, -1);
+        }
+        return coinChangeUtil(n - 1, T, coins, dp);
+    }
+
+    private int coinChangeUtil(int idx, int T, int[] coins, int[][] dp) {
+        if (T == 0) {
+            return 1;
+        }
+        if (idx == 0) {
+            if (T % coins[0] == 0) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        if (dp[idx][T] != -1) {
+            return dp[idx][T];
+        }
+        int notPick = coinChangeUtil(idx - 1, T, coins, dp);
+        int pick = 0;
+        if (coins[idx] <= T) {
+            pick = coinChangeUtil(idx, T - coins[idx], coins, dp);
+        }
+
+        return dp[idx][T] = pick + notPick;
+    }
+}
