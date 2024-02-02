666abef1ae118238e02167736f7263091ef77174
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-02-02 19:59:58 +0530

    Update LongestIncreasingSubsequence.java

--- DP/DP_ON_LIS/LongestIncreasingSubsequence.java
+++ DP/DP_ON_LIS/LongestIncreasingSubsequence.java
@@ -129,6 +129,30 @@ class Solution {
         
         return ans == Integer.MIN_VALUE ? 1 : ans ;
     }
+}
+
+//Binary Search
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+        temp.add(nums[0]);
+        int len = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > temp.get(temp.size() - 1)) {
+                temp.add(nums[i]);
+                len++;
+            } else {
+                int ind = Collections.binarySearch(temp, nums[i]);
+                if (ind < 0) {
+                    ind = -ind - 1;
+                }
+
+                temp.set(ind, nums[i]);
+            }
+        }
+        return len;
+    }
 
 
 }
