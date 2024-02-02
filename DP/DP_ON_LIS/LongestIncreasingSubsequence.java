0242029ca446eefc79aab1697d81e04fb908a6d7
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-02-02 11:01:15 +0530

    Update LongestIncreasingSubsequence.java

--- DP/DP_ON_LIS/LongestIncreasingSubsequence.java
+++ DP/DP_ON_LIS/LongestIncreasingSubsequence.java
@@ -20,7 +20,7 @@ class Solution {
     }
 }
 
-//Tabulation solution
+//memoization solution
 
 class Solution {
     public int lengthOfLIS(int[] nums) {
@@ -50,5 +50,33 @@ class Solution {
         }
 
         return dp[idx][prevIdx + 1] = Math.max(pick, notPick);
+    }
+}
+
+//Tabulation solution
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+    
+        return lengthOfLISUtil(nums);
+    }
+
+    private int lengthOfLISUtil(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n + 1][n + 1];
+
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int prevIdx = idx - 1; prevIdx >= -1; prevIdx--) {
+                int notPick = 0 + dp[idx + 1][prevIdx + 1];
+                int pick = 0;
+                if (prevIdx == -1 || nums[prevIdx] < nums[idx]) {
+                    pick = 1 + dp[idx + 1][idx + 1];
+                }
+
+                dp[idx][prevIdx + 1] = Math.max(pick, notPick);
+            }
+        }
+        return dp[0][-1 + 1];
     }
 }
