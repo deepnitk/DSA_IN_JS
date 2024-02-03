94501f0c593a1d4a924a84d69949eb826353916e
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-02-03 11:49:45 +0530

    Update LongestCommonSubsequences.java

--- DP/DP_ON_Strings/LongestCommonSubsequences.java
+++ DP/DP_ON_Strings/LongestCommonSubsequences.java
@@ -21,3 +21,34 @@ class Solution {
     }
 
 }
+
+//Memoization approach
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n][m];
+        for(int[] row: dp) {
+            Arrays.fill(row, -1);
+        }
+        return longestCommonSubsequenceUtil(text1, n - 1, text2, m - 1, dp);
+    }
+
+    private int longestCommonSubsequenceUtil(String s1, int idx1, String s2, int idx2, int[][] dp) {
+        if (idx1 < 0 || idx2 < 0) {
+            return 0;
+        }
+
+        if (dp[idx1][idx2] != -1) {
+            return dp[idx1][idx2];
+        }
+        if (s1.charAt(idx1) == s2.charAt(idx2)) {
+            return dp[idx1][idx2] = 1 + longestCommonSubsequenceUtil(s1, idx1 - 1, s2, idx2 - 1, dp);
+        }
+
+        return dp[idx1][idx2] = Math.max((0 + longestCommonSubsequenceUtil(s1, idx1 - 1, s2, idx2, dp)),
+                        (0 + longestCommonSubsequenceUtil(s1, idx1, s2, idx2 - 1, dp)));
+    }
+
+}
