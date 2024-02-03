73a2c8648130d8d6fdc97f42ff2a1252d2524db2
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-02-03 20:34:43 +0530

    Update DistinctSubset.java

--- DP/DP_ON_Strings/DistinctSubset.java
+++ DP/DP_ON_Strings/DistinctSubset.java
@@ -89,3 +89,36 @@ class Solution {
         }
     }
 }
+
+//Tabulation
+
+class Solution {
+    public int numDistinct(String s, String t) {
+
+        return numDistinctUtil(s, t);
+    }
+
+    private int numDistinctUtil(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+         for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < m + 1; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int idx1 = 1; idx1 <= n; idx1++){
+            for (int idx2 = 1; idx2 <= m; idx2++) {
+                if (s1.charAt(idx1 - 1) == s2.charAt(idx2 - 1)) {
+                    dp[idx1][idx2] = dp[idx1 - 1][idx2 - 1] + dp[idx1 - 1][idx2];
+                } else {
+                    dp[idx1][idx2] = dp[idx1 - 1][idx2];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
