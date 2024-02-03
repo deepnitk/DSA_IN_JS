e6bdf559d7b91d61f3053e25c8f524d259f17825
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-02-03 20:09:40 +0530

    Create DistinctSubset.java

---
+++ DP/DP_ON_Strings/DistinctSubset.java
@@ -0,0 +1,58 @@
+//Recursive solution
+
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        return numDistinctUtil(s, n - 1, t, m - 1);
+    }
+
+    private int numDistinctUtil(String s1, int idx1, String s2, int idx2) {
+        if (idx2 < 0) {
+            return 1;
+        }
+        if (idx1 < 0) {
+            return 0;
+        }
+
+        if (s1.charAt(idx1) == s2.charAt(idx2)) {
+            return numDistinctUtil(s1, idx1 - 1, s2, idx2 - 1) + numDistinctUtil(s1, idx1 - 1, s2, idx2);
+        } else {
+            return numDistinctUtil(s1, idx1 - 1, s2, idx2);
+        }
+    }
+}
+
+// Memoization
+
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        int[][] dp = new int[n][m];
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return numDistinctUtil(s, n - 1, t, m - 1, dp);
+    }
+
+    private int numDistinctUtil(String s1, int idx1, String s2, int idx2, int[][] dp) {
+        if (idx2 < 0) {
+            return 1;
+        }
+        if (idx1 < 0) {
+            return 0;
+        }
+
+        if (dp[idx1][idx2] != -1) {
+            return dp[idx1][idx2];
+        }
+        if (s1.charAt(idx1) == s2.charAt(idx2)) {
+            return dp[idx1][idx2] = numDistinctUtil(s1, idx1 - 1, s2, idx2 - 1, dp) + numDistinctUtil(s1, idx1 - 1, s2, idx2, dp);
+        } else {
+            return dp[idx1][idx2] = numDistinctUtil(s1, idx1 - 1, s2, idx2, dp);
+        }
+    }
+}
