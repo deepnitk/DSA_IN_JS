3073f37a54ab6420b2f0cae0eda10a33277660de
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-02-03 11:47:52 +0530

    Create LongestCommonSubsequences.java

---
+++ DP/DP_ON_Strings/LongestCommonSubsequences.java
@@ -0,0 +1,23 @@
+//Recursive approach
+
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        return longestCommonSubsequenceUtil(text1, n - 1, text2, m - 1);
+    }
+
+    private int longestCommonSubsequenceUtil(String s1, int idx1, String s2, int idx2) {
+        if (idx1 < 0 || idx2 < 0) {
+            return 0;
+        }
+
+        if (s1.charAt(idx1) == s2.charAt(idx2)) {
+            return 1 + longestCommonSubsequenceUtil(s1, idx1 - 1, s2, idx2 - 1);
+        }
+
+        return Math.max((0 + longestCommonSubsequenceUtil(s1, idx1 - 1, s2, idx2)),
+                        (0 + longestCommonSubsequenceUtil(s1, idx1, s2, idx2 - 1)));
+    }
+
+}
