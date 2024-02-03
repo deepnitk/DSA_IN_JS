7114837875c504e1d99dd5d3085d067ade0f433e
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-02-03 14:07:44 +0530

    Update LongestPaliindromicSubSequence.java

--- DP/DP_ON_Strings/LongestPaliindromicSubSequence.java
+++ DP/DP_ON_Strings/LongestPaliindromicSubSequence.java
@@ -20,3 +20,44 @@ class Solution {
                         (0 + longestCommonSubsequenceUtil(s1, idx1, s2, idx2 - 1)));
     }
 }
+
+//Tabulation approach
+
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        String reversed = new StringBuilder(s).reverse().toString();
+        return longestCommonSubsequenceUtil(s, reversed);
+    }
+
+    private int longestCommonSubsequenceUtil(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int rows[] : dp)
+            Arrays.fill(rows, -1);
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = 0;
+        }
+
+        for (int idx1 = 1; idx1 <= n; idx1++) {
+            for (int idx2 = 1; idx2 <= m; idx2++) {
+                if (s1.charAt(idx1 - 1) == s2.charAt(idx2 - 1)) {
+                    dp[idx1][idx2] = 1 + dp[idx1 - 1][idx2 - 1];
+                } else {
+                     dp[idx1][idx2] = Math.max((0 + dp[idx1 - 1][idx2]),
+                                (0 + dp[idx1][idx2 - 1]));
+                }
+
+               
+            }
+        }
+        
+        return dp[n][m];
+    }
+
+}
