416a52dcce82c1d1b26657221004f47092a06307
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-03-18 18:54:42 +0530

    Update CountDigits.js

--- Basic Math/CountDigits.js
+++ Basic Math/CountDigits.js
@@ -1,41 +1,14 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-
-import java.io.*;
-import java.util.*;
-
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while(t-- > 0)
-        {
-            int N = Integer.parseInt(read.readLine());
-            Solution ob = new Solution();
-            System.out.println(ob.evenlyDivides(N));
+public class Solution {
+    public static int countDigits(int n){
+        int count = 0;
+        int num = n;
+        while (num != 0) {
+            int digit = num % 10;
+            if (digit != 0) {
+                if (n % digit == 0) count++;
+            }
+            num = num / 10;
         }
-    }
-}
-// } Driver Code Ends
-
-
-//User function Template for Java
-
-
-class Solution{
-    static int evenlyDivides(int N){
-       int num = N;
-       int count = 0;
-       while (num > 0) {
-           int digit = num % 10;
-           if (N % digit == 0) {
-               count++;
-           }
-           num = num/10;
-       }
-       return count;
+        return count;
     }
 }
