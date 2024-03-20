dae51e55f078de0e702f9ec08886825c81f1b790
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-03-20 18:59:34 +0530

    Create isPrime.java

---
+++ Basic Math/isPrime.java
@@ -0,0 +1,16 @@
+public class Solution {
+	public static boolean isPrime(int num) {
+		//Your code goes here
+		int cnt = 0;
+		for(int i = 1; i*i <= Math.sqrt(num); i++) {
+			if (num % i == 0) {
+				cnt++;
+				if (num / i != i) cnt++;
+			}
+		}
+		if (cnt == 2) {
+			return true;
+		}
+		return false;
+	}
+}
