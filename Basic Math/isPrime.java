1c1f4d91387e98dedc386316ab4e348c09230158
ratnadeep konar <ratnadeep.konar@gmail.com>
2024-03-20 19:05:01 +0530

    Update isPrime.java

--- Basic Math/isPrime.java
+++ Basic Math/isPrime.java
@@ -2,7 +2,7 @@ public class Solution {
 	public static boolean isPrime(int num) {
 		//Your code goes here
 		int cnt = 0;
-		for(int i = 1; i*i <= Math.sqrt(num); i++) {
+		for(int i = 1; i*i <= num; i++) {
 			if (num % i == 0) {
 				cnt++;
 				if (num / i != i) cnt++;
